6690ecd131330a5a428cf7c0b8718fea1a2c7815
AmmarMahmoud1 <ammar.hendawi@gmail.com>
2023-05-23 17:14:51 +0200

    cors fixed

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.use(cors({
   credentials: true,
   origin: [
-    'http://localhost:5173',
+    'http://localhost:3000',
     /https:\/\/wd37-rubber-ducks\.netlify\.app/,
   ],
   optionsSuccessStatus: 200,
