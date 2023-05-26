a1f7cd391449224cfbfe643e7c479194258f82a3
AmmarMahmoud1 <ammar.hendawi@gmail.com>
2023-05-26 10:54:17 +0200

    pagination added

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -9,8 +9,8 @@ const jwt = require('jsonwebtoken');
 
 const getAll = async(req, res) => {
 
-  const posts = await Post.find({createdOn: { $lte: request.createdOnBefore }}).limit( 10 )
-  .sort( '-createdOn' )
+  const posts = await Post.find({}).limit( 10 )
+  .sort( '+createdOn' )
   .catch(err => res.status(500).send('Server Error'))
   res.status(200).json(posts);
 
