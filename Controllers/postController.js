e09bd65baa284cac865f0dd9d49785d21a2efbf6
riddhi <riddhivekariya3@gmail.com>
2023-06-01 14:30:12 +0200

    adding api key, lib folder and  openai file

--- Controllers/postController.js
+++ Controllers/postController.js
@@ -1,115 +1,133 @@
-const cookieParser = require('cookie-parser')
-const Post = require('../Models/post');
-const User = require('../Models/User')
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const config = require('../config');
+const cookieParser = require("cookie-parser");
+const Post = require("../Models/post");
+const User = require("../Models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const config = require("../config");
+const askAI = require("../lib/openai.js");
 
-const getPosts = async (req, res) =>{
-const posts = (await Post.find({postType:"Offer"}).catch(err => console.log(err)) );
-console.log(posts)
-res.status(200).json(posts);
+const getPosts = async (req, res) => {
+  const posts = await Post.find({ postType: "Offer" }).catch((err) =>
+    console.log(err)
+  );
+  console.log(posts);
+  res.status(200).json(posts);
+};
 
-}
+const getServices = async (req, res) => {
+  await Post.find({ category: "Services", postType: "Offer" }).catch((err) =>
+    res.status(404).json({
+      success: false,
+      error: "Internal error during fetching services!",
+    })
+  );
+};
 
-const getServices = async (req, res ) =>{
-  await Post.find({category: 'Services', postType:'Offer'}).catch(err => res.status(404).json({success : false,
-    error: "Internal error during fetching services!"}));
-}
-
-const getJobs = async (req, res ) =>{
-    await Post.find({category: 'Jobs', postType:'Offer'} )
-    .catch(err => 
-      res.status(404).json(
-         {success : false,
-               error: "Internal error during fetching services!"}));
-  
-  
-  }
-
-
-
-
+const getJobs = async (req, res) => {
+  await Post.find({ category: "Jobs", postType: "Offer" }).catch((err) =>
+    res.status(404).json({
+      success: false,
+      error: "Internal error during fetching services!",
+    })
+  );
+};
 
 const addPost = async (req, res, next) => {
   const token = req.cookies.token;
-  console.log(req.cookies)
+  console.log(req.cookies);
   // const token = req.cookies.token;
-  
+
   // if (!token) {
   //   return res.status(401).json({ message: 'Authentication required.' });
   // }
-  
+
   // try {
   //   const decoded = jwt.verify(token, config.secretKey);
   //   req.userId = decoded.userId;
 
-
-  const decoded = jwt.verify(token, 'Ammar221');
+  const decoded = jwt.verify(token, "Ammar221");
   req.userId = decoded.userId;
   console.log(req.userId);
 
   try {
-    
-    const {postType, title, category, content, Address, zipCode} = req.body;
-    const {userId} = req.userId;
-    console.log(userId + 'user id to DB')
-    const newPost = await Post.create({postType, title, category, content, Address, zipCode, userId});
-    return res.status(201).json(newPost)
+    const { postType, title, category, content, Address, zipCode } = req.body;
+    const { userId } = req.userId;
+    console.log(userId + "user id to DB");
+    const newPost = await Post.create({
+      postType,
+      title,
+      category,
+      content,
+      Address,
+      zipCode,
+      userId,
+    });
+    return res.status(201).json(newPost);
   } catch (error) {
     next(error);
   }
-}
+};
 
-const updatePost = async (req , res) =>{
- const post = await Post.findById(req.params.id);
- if(!post) {
-    res.status(400) 
-    throw new Error("Post not found!..")
- }
- if(!req.author){
-    res.status(401) 
-    throw new Error('User not found!...')
+const updatePost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (!post) {
+    res.status(400);
+    throw new Error("Post not found!..");
+  }
+  if (!req.author) {
+    res.status(401);
+    throw new Error("User not found!...");
   }
 
-  if (post.author.toString() !== req.author.id){
-    res.status(401) 
-    throw new Error('User not authorized!...')
+  if (post.author.toString() !== req.author.id) {
+    res.status(401);
+    throw new Error("User not authorized!...");
+  }
 
- }  
- 
- const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+  const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
-  })
+  });
 
   res.status(200).json(updatedPost);
+};
 
+const deletePost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
 
-}
-
-const deletePost = async (req, res) =>{
-    const post  = await Post.findById(req.params.id)
-
-    if(!post){
-     res.status(400)
-    throw new Error('Post not found!.. ')
+  if (!post) {
+    res.status(400);
+    throw new Error("Post not found!.. ");
   }
-  if(!req.author){
-    res.status(401)
-    throw new Error('User not found')
+  if (!req.author) {
+    res.status(401);
+    throw new Error("User not found");
   }
-  if(post.author.toString() !== req.author.id){
-    res.status(401)
-    throw new Error('User not authorized')
+  if (post.author.toString() !== req.author.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
 
-  await post.remove()
+  await post.remove();
 
-  res.status(200).json({ id: req.params.id })
+  res.status(200).json({ id: req.params.id });
+};
 
-
-}
+const askPost = async (req, res, next) => {
+  try {
+    const { input } = req.body;
+    const aiResponse = await askAI(input);
+    res.json({ postSays: aiResponse });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports = {
-    getJobs, getPosts, getServices, addPost, updatePost, deletePost
-}
+  getJobs,
+  getPosts,
+  getServices,
+  addPost,
+  updatePost,
+  deletePost,
+  askPost,
+};
--- server.js
+++ server.js
@@ -1,31 +1,32 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const app = express()
-const cors = require('cors');
-
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const app = express();
+const cors = require("cors");
 
 app.use(bodyParser.json());
 
-const PORT = 5000 || process.env.PORT;
+const port = process.env.PORT || 7300;
 
-const db = require('./db');
+const db = require("./db");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({
-  credentials: true,
-  origin: "*",
-  optionsSuccessStatus: 200,
-}))
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-app.use('/api/user', require('./Routes/userRoutes'))
-app.use('/api' , require('./Routes/postRoutes'))
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+);
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+app.use("/api/user", require("./Routes/userRoutes"));
+app.use("/api", require("./Routes/postRoutes"));
 
-app.listen(PORT, () => {
-    console.log(` app listening on port ${PORT}`)
-  });
+app.listen(port, () => {
+  console.log(`app listening on http://localhost:${port}`);
+});
