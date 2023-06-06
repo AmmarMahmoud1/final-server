209c455cd3a86045f84cda04a5a3abfc2db1d7b6
AmmarMahmoud1 <ammar.hendawi@gmail.com>
2023-06-06 14:25:58 +0200

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -28,7 +28,9 @@ const db = require("./db");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+});
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // ROUTES
