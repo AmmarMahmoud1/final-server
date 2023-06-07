aee84f27f1a4bc511a84e71367df9bc98c4fb241
AmmarMahmoud1 <ammar.hendawi@gmail.com>
2023-06-07 22:34:21 +0200

    fianl version with react url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 const cors = require("cors");
 const corsOptions ={
-  origin:['http://localhost:3000','https://searchandoffer1.netlify.app'
+  origin:['http://localhost:3000','https://main--searchandoffer1.netlify.app/'
   ],
   credentials:true,        
   optionSuccessStatus:200
